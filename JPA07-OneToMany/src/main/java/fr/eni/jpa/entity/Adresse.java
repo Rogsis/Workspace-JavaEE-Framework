d5f29fb429217a00f44fdfcb63a63f4fdd55ce99
CHAPELET KEVIN <kevin.chapelet2017@campus-eni.fr>
2018-11-14 09:55:57 +0100

    JPA07 - One to Many

---
+++ JPA07-OneToMany/src/main/java/fr/eni/jpa/entity/Adresse.java
@@ -0,0 +1,53 @@
+package fr.eni.jpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ADRESSES_ONETOMANY")
+public class Adresse {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String ville;
+
+	public Adresse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Adresse(String ville) {
+		super();
+		this.ville = ville;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Adresse [id=");
+		builder.append(id);
+		builder.append(", ville=");
+		builder.append(ville);
+		builder.append("]");
+		return builder.toString();
+	}
+}
